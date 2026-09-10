06d68fa91a9c7af520ce6febbc023437fe911139
Priyanka Verma <pverm157@gmail.com>
2026-09-10 15:28:07 -0400

    Update task_8_joins.py

--- sql_tasks/task_8_joins.py
+++ sql_tasks/task_8_joins.py
@@ -7,9 +7,15 @@ def get_customer_spend():
     # Task: Join Customers, Orders, and Order_Items to calculate 
     # total spend (price * quantity) per Customer Name.
     query = """
-    -- WRITE YOUR SQL HERE
-    """
+    select customer_id, price * quantity from (
+    select customer_id, order_id, price, quantity 
+    from Orders a
+    join Order_items b on a.order_id = b.order_id
+    ) as a
+    group by customer_id
     
+    """
+     
     cursor.execute(query)
     results = cursor.fetchall()
     conn.close()
